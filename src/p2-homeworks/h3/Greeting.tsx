c4305789f88eb6304d33212b2ca06cd42a68db05
Dmitrii1987 <62569977+Dmitrii1987@users.noreply.github.com>
2022-07-07 14:08:10 +0300

    refactor: add some types

--- src/p2-homeworks/h3/Greeting.tsx
+++ src/p2-homeworks/h3/Greeting.tsx
@@ -5,8 +5,8 @@ type GreetingPropsType = {
     name: any // need to fix any
     setNameCallback: any // need to fix any
     addUser: any // need to fix any
-    error: any // need to fix any
-    totalUsers: any // need to fix any
+    error: string // need to fix any
+    totalUsers: number // need to fix any
 }
 
 // презентационная компонента (для верстальщика)
